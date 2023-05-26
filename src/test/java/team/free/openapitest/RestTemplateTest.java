9e495ef207257880b41232471212e947d4fbca4d
jcw1031 <jcw001031@gmail.com>
2023-05-27 01:03:12 +0900

    위도, 경도 처리

--- src/test/java/team/free/openapitest/RestTemplateTest.java
+++ src/test/java/team/free/openapitest/RestTemplateTest.java
@@ -2,11 +2,17 @@ package team.free.openapitest;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
 public class RestTemplateTest {
+
+    private Logger log = LoggerFactory.getLogger(getClass());
 
     @Autowired
     OpenAPITest openAPITest;
@@ -19,9 +25,17 @@ public class RestTemplateTest {
 
         ResultInfo resultInfo = elevatorInfo.getResultInfo();
         String resultCode = resultInfo.getResultCode();
-        System.out.println("resultCode = " + resultCode);
+        log.info("resultCode = {}", resultCode);
 
-        Elevator elevator = elevatorInfo.getElevators().get(100);
-        System.out.println("elevator = " + elevator);
+        List<Elevator> elevators = elevatorInfo.getElevators();
+        log.info("elevators.size() = {}", elevators.size());
+
+        Elevator elevator = elevators.get(100);
+        log.info("elevator = {}", elevator);
+        String coordinates = elevator.getCoordinates();
+        String pureCoordinates = coordinates.substring(6, coordinates.length() - 1);
+        String longitude = pureCoordinates.split(" ")[0];
+        String latitude = pureCoordinates.split(" ")[1];
+        log.info("latitude: {}, longitude: {}", latitude, longitude);
     }
 }
